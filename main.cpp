1dadc026fc7d3b09dc139396afb9eb56e4a780ef
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-05 16:13:12 +1000

    Implement waiting time calculations

--- main.cpp
+++ main.cpp
@@ -10,13 +10,17 @@ using std::string;
 using std::vector;
 
 
-void fcfsSimulation();
+void fcfsSimulation(vector<int> process_id, vector<int> burst_time, vector<int> arrival_time, int numProcesses);
 
 
 int main(int argc, char** argv) {
 
-    // string processFile = "";
     std::ifstream infile;
+
+    vector<int> process_id;
+    vector<int> burst_time;
+    vector<int> arrival_time;
+    int numProcesses = 0;
 
     // Check for correct number of arguments
     if (argc != 3) {
@@ -26,31 +30,6 @@ int main(int argc, char** argv) {
 
     // Read in file
     infile.open(argv[2]);
-
-
-    // Arrays -----------------------------------
-    // int process_id[1000];
-    // int burst_time[1000];
-    // int arrival_time[1000];
-    // int num = 0;
-
-    // if (infile.fail()) {
-    //     cout << "Error - could not open file" << endl;
-    //     return EXIT_FAILURE;
-    // } else {
-    //     while (!infile.eof()) {
-    //         infile >> process_id[num] >> burst_time[num] >> arrival_time[num];
-    //         ++num;
-    //     }
-    // }
-    // infile.close();
-    // -------------------------------------------
-
-    // Vectors -----------------------------------
-
-    vector<int> process_id;
-    vector<int> burst_time;
-    vector<int> arrival_time;
 
     if (infile.fail()) {
         cout << "Error - could not open file" << endl;
@@ -68,23 +47,32 @@ int main(int argc, char** argv) {
         }
     }
 
-    cout << process_id.size() << endl;
-
     infile.close();
-    // -------------------------------------------
+
+    // cout << process_id.size() << endl;
+    numProcesses = process_id.size();
+
 
     // Check for which process scheduler
     if (argv[1] == string("-fcfs")) {
         cout << "FCFS" << endl;
-        // fcfsSimulation();
+        fcfsSimulation(process_id, burst_time, arrival_time, numProcesses);
 
         // for (int i = 0; i < process_id.size(); ++i) {
         //     cout << process_id[i] << " " << burst_time[i] << " " << arrival_time[i] << endl;
         // }
 
-        for (int i : process_id) {
-            cout << process_id[i-1] << " " << burst_time[i-1] << " " << arrival_time[i-1] <<endl;;
-        }
+        // for (int i : process_id) {
+        //     cout << process_id[i-1] << " " << burst_time[i-1] << " " << arrival_time[i-1] <<endl;;
+        // }
+
+        // int totalProcessTime = 0;
+        // for (int i : burst_time) {
+        //     totalProcessTime += burst_time[i];
+        // }
+
+        // cout << "Total process time: " << totalProcessTime << endl;
+
     } else if (argv[1] == string("-sjf")) {
         cout << "SJF" << endl;
     } else if (argv[1] == string("-rr")) {
@@ -98,6 +86,35 @@ int main(int argc, char** argv) {
 }
 
 
-void fcfsSimulation() {
+void fcfsSimulation(vector<int> process_id, vector<int> burst_time, vector<int> arrival_time, int numProcesses) {
+
+    vector<int> waiting_time;
+    vector<int> running_time;
+
+    
+    running_time.push_back(0);
+    // Waiting time of first process is 0
+    waiting_time.push_back(0);
+
+
+    // Calculate the waiting time
+    for(int i = 1; i < numProcesses; ++i) {
+
+        // Calculate the running time by adding burst time of previous process
+        running_time.push_back(running_time[i-1] + burst_time[i-1]);
+
+        // Calculate waiting time for current process
+        waiting_time.push_back(running_time[i] - arrival_time[i]);
+
+        if (waiting_time[i] < 0) {
+            waiting_time[i] = 0;
+        }
+
+        cout << "Waiting time for process " << process_id[i] << ": " << waiting_time[i] << endl;
+
+
+    }
+
+
 
 }
